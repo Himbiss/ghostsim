33da53c700fc11b05ebb2e75eb95f3e314817a02
Vincent Ortland <vincent.ortland@uni-oldenburg.de>
2014-11-05 21:15:04 +0100

    added very basic autocompletion to the editor (STRG+SPACE)

---
+++ src/ghostsimulator/AutocompleteAction.java
@@ -0,0 +1,91 @@
+package ghostsimulator;
+
+import java.awt.Point;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.beans.PropertyChangeListener;
+import java.util.HashMap;
+
+import javax.swing.Action;
+import javax.swing.JMenuItem;
+import javax.swing.JPopupMenu;
+import javax.swing.text.BadLocationException;
+
+import util.ImageLoader;
+
+
+public class AutocompleteAction implements Action {
+	private Editor editor;
+	private boolean isEnabled = true;
+	// an array of function strings for autocompletion
+	private String[] functions = {"takeFireball()",
+								  "putDownFireball()",
+								  "shootFireball()",
+								  "hasFireballs()",
+								  "fireballOnTile()",
+								  "wallInFront()",
+								  "turnLeft()",
+								  "moveForward()"};
+	private ActionListener menuListener;
+	private JPopupMenu popup;
+	
+	public AutocompleteAction(final Editor editor) {
+		this.editor = editor;
+		this.popup = new JPopupMenu();
+		this.menuListener = new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent ev) {
+				// append text to document
+				String append = ev.getActionCommand();
+				int caretPosition = editor.getCaretPosition();
+				try {
+					editor.getDocument().insertString(caretPosition, append, null);
+				} catch (BadLocationException e) {
+					System.err.println("Could not append autocompletion!");
+					e.printStackTrace();
+				}
+			}
+		};
+		// add items to popup
+		for(int i=0; i<functions.length; i++) {
+			JMenuItem item = new JMenuItem(functions[i]);
+			item.addActionListener(this);
+			item.addActionListener(menuListener);
+			popup.add(item);
+		}
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent ev) {
+		System.out.println("Action performed!");
+		Point caretPosition = editor.getCaret().getMagicCaretPosition();
+		popup.show(editor, caretPosition.x, caretPosition.y);
+	}
+
+	@Override
+	public void addPropertyChangeListener(PropertyChangeListener listener) {
+	}
+
+	@Override
+	public Object getValue(String key) {
+		return null;
+	}
+
+	@Override
+	public boolean isEnabled() {
+		return isEnabled;
+	}
+
+	@Override
+	public void putValue(String key, Object value) {
+	}
+
+	@Override
+	public void removePropertyChangeListener(PropertyChangeListener listener) {
+	}
+
+	@Override
+	public void setEnabled(boolean b) {
+		isEnabled = b;
+	}
+}
--- src/ghostsimulator/Editor.java
+++ src/ghostsimulator/Editor.java
@@ -1,8 +1,14 @@
 package ghostsimulator;
 
 import java.awt.Dimension;
+import java.awt.event.ActionEvent;
+import java.awt.event.InputEvent;
+import java.awt.event.KeyEvent;
+import java.beans.PropertyChangeListener;
 
+import javax.swing.Action;
 import javax.swing.JEditorPane;
+import javax.swing.KeyStroke;
 
 /**
  * An editor in which the user can write code that can be executed
@@ -14,6 +20,10 @@ public class Editor extends JEditorPane {
 	
 	public Editor() {
 		setPreferredSize(new Dimension(300, 0));
+		// add autocompletion to the editor
+		getInputMap().put(KeyStroke.getKeyStroke(KeyEvent.VK_SPACE, InputEvent.CTRL_DOWN_MASK), "autocomplete");
+		getActionMap().put("autocomplete", new AutocompleteAction(this));
 	}
+
 	
 }
